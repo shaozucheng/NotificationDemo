c81f60b855520dfd79cfd4835291777a58917898
shaozucheng <shaozucheng@likingfit.com>
2017-12-15 15:16:12 +0800

    简述：首次提交demo
    背景信息：
    
    修改内容：

---
+++ app/src/main/java/com/notification/MainActivity.java
@@ -0,0 +1,124 @@
+package com.notification;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RemoteViews;
+import android.widget.TextView;
+
+import java.util.Random;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import push.NotificationManager;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.button01)
+    TextView mButton01;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        // 183 2141 0040
+    }
+
+
+    @OnClick({R.id.button01, R.id.button02, R.id.button03, R.id.button04})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button01://第一种方式
+                showNotification();
+                break;
+            case R.id.button02://第二种方式
+                showNotion();
+                break;
+            case R.id.button03://自定义view的方式
+                showCustomNotification(this);
+                break;
+            case R.id.button04:
+                showBigNotification();
+                break;
+        }
+    }
+
+
+    private void showBigNotification(){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,null)
+                        .setSmallIcon(R.mipmap.ic_launcher_round)
+                        .setContentTitle("标题")
+                        .setContentText("大标题内容")
+                        .setDefaults(Notification.DEFAULT_ALL)
+                        // 该方法在Android 4.1之前会被忽略
+                        .setStyle(new NotificationCompat.BigTextStyle()
+                                .bigText("大标题"));
+                        //添加Action Button
+
+
+    }
+
+    /**
+     * 第一种方式，普通通知
+     */
+    private void showNotification() {
+        Intent resultIntent = new Intent(this, SecondActivity.class);
+        resultIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        //(new Random().nextInt(1000) 这个地方这么写的原因，是部分低版本的不能跳转，比如说小米3
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(this, (new Random().nextInt(1000)),
+                resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationManager.showNotification(this, getString(R.string.app_name),
+                "alert 消息", resultPendingIntent);
+    }
+
+    /**
+     * 第二种方式的普通通知
+     */
+    private void showNotion() {
+        Intent resultIntent = new Intent(this, SecondActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(SecondActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        resultIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        NotificationManager.showNotification(this, getString(R.string.app_name),
+                "alert 消息", resultPendingIntent);
+
+    }
+
+
+    /**
+     * 自定义view 的普通通知
+     *
+     * @param context
+     */
+    private static void showCustomNotification(Context context) {
+        //自定义显示布局
+        RemoteViews contentViews = new RemoteViews(context.getPackageName(), R.layout.custom_notification);
+        //通过控件的Id设置属性
+        contentViews.setImageViewResource(R.id.ImageView, R.mipmap.ic_launcher);
+        contentViews.setTextViewText(R.id.title, "自定义通知标题");
+        contentViews.setTextViewText(R.id.content, "自定义通知内容");
+
+        Intent resultIntent = new Intent(context, SecondActivity.class);
+        resultIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        //(new Random().nextInt(1000) 这个地方这么写的原因，是部分低版本的不能跳转，比如说小米3
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(context, (new Random().nextInt(1000)),
+                resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationManager.showCustomNotification(context, contentViews, "ticker", resultPendingIntent);
+
+    }
+
+
+}
---
+++ app/src/main/java/push/NotificationManager.java
@@ -0,0 +1,137 @@
+package push;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.widget.RemoteViews;
+
+import com.notification.R;
+
+
+/**
+ * 作者： chengcheng
+ * 时间： 17/12/4- 下午3:32O
+ * 描述： 通知管理类
+ */
+
+public class NotificationManager {
+    private static int NOTICE_ID_DEFAULT = 100000;
+
+    public NotificationManager() {
+    }
+
+    /**
+     * 基本展示通知方法
+     *
+     * @param ctx      上下文
+     * @param noticeId 通知id
+     * @param title    通知title
+     * @param message  通知内容
+     * @param intent   PendingIntent
+     */
+    private static void showNotify(Context ctx, int noticeId, String title, String message, PendingIntent intent) {
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(ctx, null);
+        mBuilder.setSmallIcon(R.mipmap.ic_launcher);
+        mBuilder.setContentTitle(title);
+        mBuilder.setContentText(message);
+        mBuilder.setContentIntent(intent);
+        mBuilder.setAutoCancel(true);
+        mBuilder.setTicker(title);
+        mBuilder.setPriority(NotificationManagerCompat.IMPORTANCE_HIGH);
+        Notification notification = mBuilder.build();
+        notification.flags = Notification.FLAG_AUTO_CANCEL;
+        notification.defaults |= Notification.DEFAULT_SOUND;//默认声音
+        notification.defaults |= Notification.DEFAULT_LIGHTS;//默认闪烁
+        android.app.NotificationManager notificationManager = (android.app.NotificationManager) ctx.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (notificationManager != null) {
+            notificationManager.notify(noticeId, notification);
+        }
+    }
+
+
+    /**
+     * 设置自定义通知 view
+     *
+     * @param ctx         上下文
+     * @param remoteViews 自定义view
+     * @param noticeId    通知id
+     * @param ticker      ticker
+     * @param intent      PendingIntent
+     */
+    private static void showCustomNotify(Context ctx, RemoteViews remoteViews, int noticeId, String ticker, PendingIntent intent) {
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(ctx, null);
+        mBuilder.setSmallIcon(R.mipmap.ic_launcher);
+        mBuilder.setContentIntent(intent);
+        mBuilder.setAutoCancel(true);
+        mBuilder.setContent(remoteViews);
+        mBuilder.setTicker(ticker);
+        mBuilder.setPriority(NotificationManagerCompat.IMPORTANCE_HIGH);
+        Notification notification = mBuilder.build();
+        notification.flags = Notification.FLAG_AUTO_CANCEL;
+        notification.defaults |= Notification.DEFAULT_SOUND;//默认声音
+        notification.defaults |= Notification.DEFAULT_LIGHTS;//默认闪烁
+        android.app.NotificationManager notificationManager = (android.app.NotificationManager) ctx.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (notificationManager != null) {
+            notificationManager.notify(noticeId, notification);
+        }
+    }
+
+
+    /**
+     * 展示通知默认方法
+     *
+     * @param ctx     上下文对象
+     * @param title   通知标题
+     * @param message 通知内容
+     * @param intent  PendingIntent
+     */
+    public static void showNotification(Context ctx, String title, String message, PendingIntent intent) {
+        showNotify(ctx, ++NOTICE_ID_DEFAULT, title, message, intent);
+    }
+
+    /**
+     * 自定义通知 noticeId 时调用该方法
+     *
+     * @param ctx      上下文对象
+     * @param noticeId 自定义通知id 必须小于 默认值（100000）
+     * @param title    通知标题
+     * @param message  通知内容
+     * @param intent   PendingIntent对象
+     */
+    public static void showNotification(Context ctx, int noticeId, String title, String message, PendingIntent intent) {
+        if (noticeId > NOTICE_ID_DEFAULT) {
+            throw new IllegalArgumentException("自定义的noticeId 必须小于" + NOTICE_ID_DEFAULT);
+        }
+        showNotify(ctx, noticeId, title, message, intent);
+    }
+
+
+    /**
+     * 展示自定义通知默认方法
+     *
+     * @param ctx         上下文
+     * @param remoteViews 自定义布局
+     * @param ticker      ticker
+     * @param intent      PendingIntent对象
+     */
+    public static void showCustomNotification(Context ctx, RemoteViews remoteViews, String ticker, PendingIntent intent) {
+        showCustomNotify(ctx, remoteViews, ++NOTICE_ID_DEFAULT, ticker, intent);
+    }
+
+
+    /**
+     * 自定义通知 noticeId 时调用该方法
+     *
+     * @param ctx         上下文
+     * @param remoteViews 自定义布局
+     * @param noticeId    通知id
+     * @param ticker      ticker
+     * @param intent      PendingIntent对象
+     */
+    public static void showCustomNotification(Context ctx, RemoteViews remoteViews, int noticeId, String ticker, PendingIntent intent) {
+        showCustomNotify(ctx, remoteViews, noticeId, ticker, intent);
+    }
+
+}
