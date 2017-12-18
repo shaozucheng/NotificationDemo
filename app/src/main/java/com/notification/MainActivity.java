da2067dd9560d3373303dd371a095c83ccef9d98
shaozucheng <shaozucheng@likingfit.com>
2017-12-18 09:44:33 +0800

    简述：修改信息
    背景信息：
    
    修改内容：

--- app/src/main/java/com/notification/MainActivity.java
+++ app/src/main/java/com/notification/MainActivity.java
@@ -30,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        // 183 2141 0040
     }
 
 
